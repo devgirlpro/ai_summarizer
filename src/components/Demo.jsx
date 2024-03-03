5768e04184824ccef9e9001946dc7b29d2db3133
devgirlpro <agalidari@gmail.com>
2024-03-03 18:46:30 +0100

    added article .js and store.js

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,16 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
-
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+
+   // RTK lazy query
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+
+  const submitHandler = async (e) => {
+     e.preventDefault();
+
+    //fetch summary API request
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      // update state and local storage
+      setArticle(newArticle);
+
+      console.log(newArticle)
+    }
+  };
+
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* search */}
       <div className='flex flex-col w-full gap-2' >
         <form
           className='relative flex justify-center items-center' 
-          onSubmit={() => {}}
+          onSubmit={submitHandler}
           >
             <img src={linkIcon}
                  alt="limk_icon" 
@@ -19,8 +45,8 @@ const Demo = () => {
             <input 
               type="url"
               placeholder="Enter a URL" 
-              value=""
-              onChange={() => {}}
+              value={article.url}
+              onChange={(e) => setArticle({ ...article, url: e.target.value })}
               required
               className="url_input peer "  // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
             />
